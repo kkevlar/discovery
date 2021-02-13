7a365dd0087f9b4bd45ce918c1206c68ba26a6a0
Kevin Kellar <kellar.kevin@gmail.com>
2021-02-12 17:15:13 -0800

    dubs

--- src/08-leds-again/auxiliary/src/lib.rs
+++ src/08-leds-again/auxiliary/src/lib.rs
@@ -7,13 +7,13 @@ extern crate panic_itm; // panic handler
 
 pub use cortex_m::asm::bkpt;
 pub use cortex_m_rt::entry;
-pub use f3::hal::stm32f30x::{gpioc, rcc};
+pub use f3::hal::stm32f30x::{gpioa, gpioc, rcc};
 
-use f3::hal::stm32f30x::{self, GPIOE, RCC};
+use f3::hal::stm32f30x::{self, GPIOA, GPIOE, RCC};
 
-pub fn init() -> (&'static gpioc::RegisterBlock, &'static rcc::RegisterBlock) {
+pub fn init() -> (&'static gpioa::RegisterBlock, &'static gpioc::RegisterBlock, &'static rcc::RegisterBlock) {
     // restrict access to the other peripherals
     (stm32f30x::Peripherals::take().unwrap());
 
-    unsafe { (&*GPIOE::ptr(), &*RCC::ptr()) }
+    unsafe { (&*GPIOA::ptr(), &*GPIOE::ptr(), &*RCC::ptr()) }
 }
--- src/08-leds-again/src/main.rs
+++ src/08-leds-again/src/main.rs
@@ -1,17 +1,27 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
-use aux8::entry;
+use core::borrow::Borrow;
 
-#[entry]
-fn main() -> ! {
-    let (gpioe, rcc) = aux8::init();
+use aux8::{entry, gpioc::RegisterBlock};
 
-    // TODO initialize GPIOE
+fn clear_all(gpioe: &RegisterBlock)
+{
+gpioe.odr.write(|w| {
+        w.odr8().clear_bit();
+        w.odr9().clear_bit();
+        w.odr10().clear_bit();
+        w.odr11().clear_bit();
+        w.odr12().clear_bit();
+        w.odr13().clear_bit();
+        w.odr14().clear_bit();
+        w.odr15().clear_bit()
+});
+}
 
-    // Turn on all the LEDs in the compass
-    gpioe.odr.write(|w| {
+fn set_all(gpioe: &RegisterBlock)
+{
+gpioe.odr.write(|w| {
         w.odr8().set_bit();
         w.odr9().set_bit();
         w.odr10().set_bit();
@@ -20,9 +30,59 @@ fn main() -> ! {
         w.odr13().set_bit();
         w.odr14().set_bit();
         w.odr15().set_bit()
+});
+}
+
+
+#[entry]
+fn main() -> ! {
+    let (gpioa, gpioe, rcc) = aux8::init();
+
+  
+  // enable the GPIOE peripheral
+    rcc.ahbenr.modify(|_, w| w.iopeen().set_bit());
+    rcc.ahbenr.modify(|_, w| w.iopaen().set_bit());
+
+    // configure the pins as outputs
+    gpioe.moder.modify(|_, w| {
+        w.moder8().output();
+        w.moder9().output();
+        w.moder10().output();
+        w.moder11().output();
+        w.moder12().output();
+        w.moder13().output();
+        w.moder14().output();
+        w.moder15().output()
     });
 
-    aux8::bkpt();
 
-    loop {}
+    gpioa.moder.modify(|_, w| 
+        w.moder0().input()
+    );
+
+    unsafe{
+    gpioa.pupdr.modify(|_, w| 
+        w.pupdr0().bits(2)
+    );
+
+}
+
+    loop{
+        clear_all(gpioe);
+ 
+   
+    while gpioa.idr.read().idr0().bit()
+    {
+
+    }
+ 
+    set_all(gpioe);
+
+    while !gpioa.idr.read().idr0().bit()
+    {
+
+    }
+
+
+}
 }
